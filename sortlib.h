0c79dc2667d2ca96483d0872043482cf6bbc43b4
Hadeer Abdelnasser <101938448+HadeerAbdelnasser204@users.noreply.github.com>
2023-03-18 22:50:11 +0200

    Create

---
+++ sortlib.h
@@ -0,0 +1,22 @@
+//
+// Created by A plus on 3/18/2023.
+//
+
+#ifndef DATA_S_SORTING_H
+#include <iostream>
+#include <algorithm>
+using namespace std;
+namespace sortlib
+{using namespace sortlib;
+    void insertionSort(int Array[], int size);
+    void selectionSort(int array[], int size);
+    void bubbleSort(int array[], int size);
+    void shellSort(int Array[], int size);
+    void mergeSort(int Array[], int left, int right);
+    void quickSort(int array[], int begin, int end);
+    void countSort(int array[], int size);
+
+}
+#define DATA_S_SORTING_H
+
+#endif //DATA_S_SORTING_H
